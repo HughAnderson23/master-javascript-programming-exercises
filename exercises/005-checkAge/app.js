e0f20271981c09499b149046b17ac97f17c8e485
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-10-30 23:46:17 +0000

    first commit

--- exercises/005-checkAge/app.js
+++ exercises/005-checkAge/app.js
@@ -1,4 +1,9 @@
 function checkAge(name, age) {
   // your code here
-  
+
+ 
+  if (age < 21) return `Go Home, ${name}!`
+  else return `Welcome, ${name}`
 }
+let output = checkAge('Adrian', 19);
+console.log(output);
