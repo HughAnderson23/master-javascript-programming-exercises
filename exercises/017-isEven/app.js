2b2410846bcf2c8fc2c418717469f63b2198732f
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-11-02 00:51:56 +0000

    question 25

--- exercises/017-isEven/app.js
+++ exercises/017-isEven/app.js
@@ -1,1 +1,7 @@
 // Write your function here
+function isEven(num) {
+    return num % 2 ;
+}
+
+let output = isEven(13);
+console.log(output)
--- exercises/020-isEitherEven/app.js
+++ exercises/020-isEitherEven/app.js
@@ -1,1 +1,6 @@
 // Write your function here
+function isEitherEven(num1, num2) {
+    return(num1 % 2 ===0)||(num2 % 2 ===0);
+}
+let output = isEitherEven(1,1);
+console.log(output);
